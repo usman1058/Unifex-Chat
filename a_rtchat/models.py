61a1e5bc454ab3f5394fdf736b18d20b9eb716bc
usman1058 <techinfo1058@gmail.com>
2024-10-15 07:36:40 -0700

    first commit

---
+++ a_rtchat/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class ChatGroup(models.Model):
+    group_name =models.CharField(max_length=124,unique=True)
+
+    def __str__(self):
+        return self.group_name
+
+
+
+
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE, related_name='chat_messages')
+    auther=models.ForeignKey(User,on_delete=models.CASCADE)
+    body=models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def  __str__(self):
+        return f'{self.auther.username} : {self.body}'
+
+    class META:
+        ordering = ('-created',)  # newest messages first
---
+++ a_rtchat/urls.py
@@ -0,0 +1,8 @@
+from django.urls  import path
+from .views import chat_view
+
+
+
+urlpatterns=[
+path('',chat_view,name="home")
+]
