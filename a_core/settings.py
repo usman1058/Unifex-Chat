aa9ae3debeb5d34476bb5f910a39a0a090534145
usman1058 <techinfo1058@gmail.com>
2024-10-17 15:41:32 +0500

    run

--- a_core/settings.py
+++ a_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercal.app']
 
 CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 
