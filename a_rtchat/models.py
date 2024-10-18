986014d096565e44c2bb455f1656b246e03f0614
usman1058 <techinfo1058@gmail.com>
2024-10-18 15:55:03 +0500

    run

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+import shortuuid
 
 # Create your models here.
 class ChatGroup(models.Model):
-    group_name =models.CharField(max_length=124,unique=True)
-    users_online = models.ManyToManyField(User,related_name='online_in_groups',blank=True)
+    group_name=models.CharField(max_length=124, unique=True, default=shortuuid.uuid)
+    users_online = models.ManyToManyField(User,related_name='online_in_groups',blank=True )
+    members=models.ManyToManyField(User,related_name='chat_groups', blank=True)
+    is_private=models.BooleanField(default=False)
 
     def __str__(self):
         return self.group_name
--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import *
@@ -6,10 +7,21 @@ from .forms import *
 
 # Create your views here.
 @login_required
-def chat_view(request):
-    chat_group = get_object_or_404(ChatGroup, group_name='Public-chat')
+def chat_view(request,chatroom_name='Public-chat'):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
     chat_messages=chat_group.chat_messages.all()[:30]
     form=ChatMessageCreateForm()
+
+    other_user=None
+    if chat_group.is_private:
+        if request.user not in chat_group.members.all():
+            raise Http404()
+        for member in chat_group.members.all():
+            if member != request.user:
+                other_user=member
+                break
+
+
 
     if request.htmx:
         form = ChatMessageCreateForm(request.POST)
@@ -23,8 +35,35 @@ def chat_view(request):
                 'user':request.user
             }
             return render(request, 'a_rtchat/partials/chat_message_p.html', context) 
-        
-    return render(request, 'a_rtchat/chat.html',{
-        'chat_messages': chat_messages,
+    
+    context={
+         'chat_messages': chat_messages,
         'form': form,
-    })
+        'other_user':other_user,
+        'chatroom_name': chatroom_name,
+    }
+
+    return render(request, 'a_rtchat/chat.html',context)
+
+@login_required
+def get_or_create_chatroom(request,username):
+    if request.user.username == username:
+        return redirect('home')
+    
+    other_user=User.objects.get(username = username)
+    my_chatroom = request.user.chat_groups.filter(is_private=True)
+
+    if my_chatroom.exists():
+        for chatroom in my_chatroom:
+            if other_user in chatroom.members.all():
+                chatroom = chatroom
+                break
+            else:
+                chatroom=ChatGroup.objects.create(is_private=True)
+                chatroom.members.add(other_user,request.user)
+    else:
+         chatroom=ChatGroup.objects.create(is_private=True)
+         chatroom.members.add(other_user,request.user)    
+
+    return redirect("chatroom", chatroom.group_name)       
+   
